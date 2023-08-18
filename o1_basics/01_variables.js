852a5270c6e90260883c6f1bc8e0ddb5e3c20a25
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-18 07:37:33 +0530

    Create variable let const var

---
+++ o1_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="raj.raviraj0@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2 not allowed
+console.log(accountId);
+
+accountEmail="ravi@gmail.com"
+accountPassword="2356"
+accountCity="ranchi"
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
