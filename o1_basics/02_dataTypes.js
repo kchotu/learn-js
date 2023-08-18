f29664e9240143cd8c29599140ca0289fd4ee048
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-18 08:18:05 +0530

    datatypes

---
+++ o1_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code newer version
+// alert(3 +3) // we are using nodejs not browers
+console.log(3+3)
+console.log('ravi singh');
+
+console.log(typeof "ravi singh");
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object type
