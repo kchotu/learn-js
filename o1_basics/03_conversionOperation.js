07fdcec519b3f4b5bb92ea88d1b686be6cef51c7
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-19 08:21:30 +0530

    Update 03_conversionOperation.js

--- o1_basics/03_conversionOperation.js
+++ o1_basics/03_conversionOperation.js
@@ -6,12 +6,12 @@ let score=undefined
 //console.log(typeof(score)); 
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //note
 //1 => true; 0 =>False
@@ -19,5 +19,21 @@ console.log(booleanIsLoggedIn);
 //"ravi" => true
 
 let someNumber=33
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber);
+
+//******************************** Operation **************//
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1= "hello"
+let str2="ravi"
+let str3= str1 + str2
+console.log(str3);
