66206af2756cb2d5a596d1fa1ca310f3315228c0
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-19 08:01:56 +0530

    conversion of datatype

---
+++ o1_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+//let score ="33aaaa"
+//let score=null
+let score=undefined
+//let score=true
+//console.log(typeof score);
+//console.log(typeof(score)); 
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//note
+//1 => true; 0 =>False
+//"" => flase
+//"ravi" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
