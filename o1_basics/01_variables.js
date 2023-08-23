e352c7f8636814d3f10fdcaa9072a1c183a02ff4
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-23 06:32:31 +0530

    Update 01_variables.js

--- o1_basics/01_variables.js
+++ o1_basics/01_variables.js
@@ -2,7 +2,8 @@ const accountId=144553
 let accountEmail="raj.raviraj0@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
-let accountState;
+let accountState=null;
+
 
 //accountId=2 not allowed
 console.log(accountId);
@@ -11,4 +12,5 @@ accountEmail="ravi@gmail.com"
 accountPassword="2356"
 accountCity="ranchi"
 
-console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+//console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+console.log(typeof accountState );
