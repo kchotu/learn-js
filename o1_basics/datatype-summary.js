2c29324112906cb7e1b13f7170dd1182f5e0296c
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-23 08:02:48 +0530

    Update datatype-summary.js

--- o1_basics/datatype-summary.js
+++ o1_basics/datatype-summary.js
@@ -1,4 +1,4 @@
-// datatype 
+// datatype  primitive
  const score=100
  const scoreValue=100.3
  const isLoggedIn=false
@@ -11,3 +11,18 @@
  console.log(id===anotherId);
 
  const bigNumber =1233365544n
+
+ //non Primtive datatype 
+
+ // array ,object, functions
+ const hero=["ravi","raj","singh"];
+
+ let myObj={
+    name: "ravisingh",
+    age:  26,
+ }
+ 
+ const myFunction =function(){
+    console.log("hello word javascript ")
+ }
+
