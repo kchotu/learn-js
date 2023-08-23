4166609d08a21199b8cd755fe43ea0cfdd88a037
RAVI SINGH <40244892+kchotu@users.noreply.github.com>
2023-08-23 07:03:23 +0530

    Create datatype-summary.js

---
+++ o1_basics/datatype-summary.js
@@ -0,0 +1,13 @@
+// datatype 
+ const score=100
+ const scoreValue=100.3
+ const isLoggedIn=false
+ const outsideTemp=null
+ let userEmail;
+
+
+ const id= Symbol('123')
+ const anotherId=Symbol('123')
+ console.log(id===anotherId);
+
+ const bigNumber =1233365544n
